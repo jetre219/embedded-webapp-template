e86f5b54fed9130adeb65b35569308d0efadc1b3
Bruno Gagnon-Adam <bgagnonadam@gmail.com>
2016-07-19 22:34:32 -0400

    Added a rest-assured integration test.

---
+++ src/test/java/com/bgagnonadam/telephony/ws/ContactResourceIT.java
@@ -0,0 +1,35 @@
+package com.bgagnonadam.telephony.ws;
+
+import com.bgagnonadam.TelephonyWsMain;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.hamcrest.Matchers;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static io.restassured.RestAssured.get;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ContactResourceIT {
+
+    @Before
+    public void setUp() throws Exception {
+        Thread t = new Thread() {
+            public void run() {
+                try {
+                    TelephonyWsMain.main(new String[]{});
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        t.setDaemon(true);
+        t.start();
+    }
+
+    @Test
+    public void givenContacts_whenGetAllContacts_thenContactsReturned() {
+        get("/api/telephony/contacts").then().body("name", Matchers.hasItem("Steve Jobs"));
+    }
+}
